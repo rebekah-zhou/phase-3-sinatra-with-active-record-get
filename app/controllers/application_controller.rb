6cff26080f76938ba7e532e67b95c1e91db440c8
Rebekah Zhou <rebekahzhou@gmail.com>
2022-03-07 13:32:48 -0800

    commit tree 5464592cf026a1c49948958a8e0f4230779e1409

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  set :default_content_type, 'application/json'
+
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+    # send a JSON-formatted response of the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
